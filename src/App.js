918f812ff8ccbda5f9f8ec8daab2255f887b5012
unknown <you@example.com>
2021-08-19 22:13:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+
+        axios.get("https://alemdocodigo.herokuapp.com/list_posts")
+            .then((response) => {
+                setPosts(response.data.posts)
+                setLoading(false)
+            })
+            // .catch(() => {
+            //     console.log("bug")
+            // })
+    }, [])
+
+    if (loading) {
+        return ( <
+            h1 > Loading < /h1>
+        )
+    }
+
+    return ( <
+        div className = "app" >
+
+        <
+        div className = "cards" > {
+            posts.map((post, key) => {
+                return ( <
+                    div className = "card"
+                    key = { key } >
+                    <
+                    div className = "card-body" >
+                    <
+                    h1 > { post.title } < /h1>   <
+                    div className = "line" > < /div>  <
+                    h2 > { post.content } < /h2>  < /
+                    div > <
+                    /div>
+
+                    <
+                    /div>
+                )
+            })
+        }
+
+
+
+        <
+        /div>
+    )
+
 }
 
 export default App;
